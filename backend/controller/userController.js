64bfbd88844c51b93929473c499cb4a618d89b35
Lalit Khairnar <lalitkhairnar93@gmail.com>
2025-02-07 00:29:41 +0530

    final

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 // Create a new user
 export const createUser = async (req, res) => {
@@ -42,7 +42,7 @@ export const loginUser = async (req, res) => {
       return res.status(400).json({ error: 'Invalid email or password' });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcryptjs.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ error: 'Invalid email or password' });
     }
