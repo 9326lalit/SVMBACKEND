27389ddc0a9b4a0ebb0b1f9e6da4937926d28dad
Satish Umagol <umagol97@gmail.com>
2025-02-26 09:07:36 +0530

    if amount is 0 that means use used free coupe

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -9,7 +9,17 @@ import Order from "../models/razorpay.js";
             amount: amount * 100, // Amount in paise
             currency: currency || "INR",
         };
-
+        if(amount == 0){
+            const newOrder = new Order({
+                razorpayOrderId: 0,
+                userId,
+                amount,
+                currency,
+                status: "created",
+            });
+            await newOrder.save();
+            return res.status(201).json({ success: true, order: newOrder, isAmountZero: true, message: "Zero amount order created successfully" });
+        }
         // Create Razorpay order
         const order = await razorpayInstance.orders.create(options);
 
@@ -24,7 +34,7 @@ import Order from "../models/razorpay.js";
 
         await newOrder.save();
 
-        res.status(201).json({ success: true, order: newOrder });
+        res.status(201).json({ success: true, isAmountZero: false, order: newOrder });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
